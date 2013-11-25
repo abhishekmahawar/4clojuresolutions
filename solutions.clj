24e26a00e6046dfd42fc506c0d472bb10d92c2c8
Abhishek <mahawar.abhi@gmail.com>
2013-11-25 12:37:36 +0530

    Update solutions.clj

--- solutions.clj
+++ solutions.clj
@@ -1,1 +1,70 @@
-;solution to 4clojure problems
+;solution to 4clojure problems (4clojure.com)
+
+;problem 1.
+true
+
+;problem 2.
+4
+
+;problem 3.
+"HELLO WORLD"
+
+;problem 4.
+:a :b :c
+
+;problem 5.
+'(1 2 3 4)
+'(1 2  3 4)
+
+;problem 6.
+:a :b :c
+
+;problem 7.
+[1 2 3 4]
+[1 2 3 4]
+  
+;problem 8.
+#{:a :b :c :d}
+#{:a :b :c :d}
+
+;problem 9.
+2
+
+;problem 10.
+20
+20
+
+;problem 11.
+{:b 2}
+
+;problem 12.
+3
+3
+3
+
+;problem 13.
+[20 30 40]
+
+;problem 14.
+8
+8
+8
+8
+
+;problem 15.
+(fn double-a-number [x] (* x 2))
+
+;problem 16.
+(fn hw [name] (str "Hello, " name "!"))
+
+;problem 17.
+'(6 7 8)
+
+;problem 18.
+'(6 7)
+
+;problem 19.
+(fn [x] (first (reverse x)))
+
+;problem 20.
+(fn [x] (first (rest (reverse x))))
